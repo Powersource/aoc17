2aa18ad001547647083d991c2f696358ac7d406a
Jacob Karlsson <jacob.karlsson95@gmail.com>
2017-12-25 16:40:40 +0100

    Finish day 1 part 2

--- one/src/main.rs
+++ one/src/main.rs
@@ -7,6 +7,12 @@ fn main() {
     let mut in_string = String::new();
     input.read_to_string(&mut in_string).unwrap();
     println!("Answer: {}", solve(&in_string));
+
+    //let file = File::open("in.txt").unwrap();
+    //let mut input = BufReader::new(file);
+    //let mut in_string = String::new();
+    //input.read_to_string(&mut in_string).unwrap();
+    println!("Answer: {}", solve2(&in_string));
 }
 
 fn solve(input: &str) -> i32 {
@@ -21,9 +27,19 @@ fn solve(input: &str) -> i32 {
     sum
 }
 
+fn solve2(input: &str) -> i32 {
+    let input = input.as_bytes();
+    let (a, b) = input.split_at(input.len() / 2);
+    let mut sum: i32 = 0;
+    a.iter().zip(b).for_each(|tup| if tup.0 == tup.1 {
+        sum += (i32::from(*tup.0) - '0' as i32) * 2
+    });
+    sum
+}
+
 #[cfg(test)]
 mod tests {
-    use super::solve;
+    use super::{solve, solve2};
 
     #[test]
     fn one_two() {
@@ -43,5 +59,30 @@ mod tests {
     #[test]
     fn nines() {
         assert_eq!(solve("91212129"), 9);
+    }
+
+    #[test]
+    fn one_two_2() {
+        assert_eq!(solve2("1212"), 6);
+    }
+
+    #[test]
+    fn mirror() {
+        assert_eq!(solve2("1221"), 0);
+    }
+
+    #[test]
+    fn counting() {
+        assert_eq!(solve2("123425"), 4);
+    }
+
+    #[test]
+    fn same() {
+        assert_eq!(solve2("123123"), 12);
+    }
+
+    #[test]
+    fn ones() {
+        assert_eq!(solve2("12131415"), 4);
     }
 }
